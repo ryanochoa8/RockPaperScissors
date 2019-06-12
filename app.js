85ccd8ab77f8bb4db69a35759c52fa09dd7e56f9
ryanochoa8 <ryan.ochoa888@gmail.com>
2019-06-12 16:34:41 -0600

    everything else

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
+let gameResult = ["Player 1 Wins!!!", "Computer Wins...", "It's a tie"]
+
 function play(playerChoice) {
+  let playerPoint = 0
+  let computerPoint = 0
+
 
   let index = Math.floor(Math.random() * 3);
 
-  let computerOptions = ["rock", "paper", "scissors"]
+  let computerOptions = ["ROCK", "PAPER", "SCISSORS"]
   let computerChoice = computerOptions[index]
-
-  console.log("Computer played " + computerChoice)
 
 
   if (playerChoice === computerChoice) {
-  } else if (playerChoice === "rock" && computerChoice === "scissors") {
-  } else if (playerChoice === "paper" && computerChoice === "rock") {
-  } else if (playerChoice === "scissors" && computerChoice === "paper") {
-  } else if (playerChoice === "rock" && computerChoice === "paper") {
-  } else if (playerChoice === "paper" && computerChoice === "scissors") {
-  } else if (playerChoice === "scissors" && computerChoice === "rock") {
+    playerPoint++;
+    computerPoint++
+  } else if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
+    playerPoint++
+  } else if (playerChoice === "PAPER" && computerChoice === "ROCK") {
+    playerPoint++
+  } else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
+    playerPoint++
+  } else if (playerChoice === "ROCK" && computerChoice === "PAPER") {
+    computerPoint++
+  } else if (playerChoice === "PAPER" && computerChoice === "SCISSORS") {
+    computerPoint++
+  } else if (playerChoice === "SCISSORS" && computerChoice === "ROCK") {
+    computerPoint++
   }
-  update(playerChoice, computerChoice)
+  update(playerChoice, computerChoice, playerPoint, computerPoint);
 }
 
-function update(playerChoice, computerChoice) {
+
+
+
+function update(playerChoice, computerChoice, playerPoint, computerPoint) {
   document.getElementById("player-rps").innerHTML = playerChoice
   document.getElementById("computer-rps").innerHTML = computerChoice
+  if (playerPoint > computerPoint) {
+    document.getElementById("game-result").innerHTML = gameResult[0]
+    document.getElementById("game-result").style.color = "green"
+  } else if (playerPoint < computerPoint) {
+    document.getElementById("game-result").innerHTML = gameResult[1]
+    document.getElementById("game-result").style.color = "red"
+  } else if (playerPoint = computerPoint) {
+    document.getElementById("game-result").innerHTML = gameResult[2]
+    document.getElementById("game-result").style.color = "blue"
+  }
 }
+
